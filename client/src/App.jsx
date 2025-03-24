4632d3e06a7b79ef9d9cdb8d08f63c0663611782
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2025-03-24 00:39:55 +0000

    add data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,24 +8,19 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Blogging Blogger</h1>
+      <h1 className="text-5xl font-bold">Blogging Blogger</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          className="text-lg text-fuchsia-800 font-bold underline  hover:text-amber-500 border-2 border-dashed ..."
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-4xl text-fuchsia-800 font-bold  hover:text-amber-500 underline">
         Click on the Vite and React logos to learn more
       </p>
     </>
